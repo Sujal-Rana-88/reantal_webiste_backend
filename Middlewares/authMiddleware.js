fb19261836b6fb32a512573bc7e26a93c406348d
Sujal Rana <118067837+Sujal-Rana-88@users.noreply.github.com>
2024-11-01 22:26:03 +0530

    commit message

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ const verifyToken = (req, res, next) => {
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-
+ 
             return res.status(401).json({ error: 'Invalid or expired token' });
         }
 
